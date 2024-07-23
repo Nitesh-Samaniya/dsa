1d96836716eb2db882fc7f4112e7d1ba9e6800a4
Nitesh-Samaniya <niteshsamaniya97@gmail.com>
2024-07-23 22:30:54 +0530

    reverse array

---
+++ array/1.reverse_array.js
@@ -0,0 +1,39 @@
+// first way
+
+// function reverseArrayExtraArray(arr){
+//     const reveserdArr = [];
+//     for(let i=arr.length-1 ; i>=0 ; i--){
+//         reveserdArr.push(arr[i]);
+//     }
+//     console.log(reveserdArr);
+// }
+
+
+// second way
+
+// function reverseArrayExtraArray(arr){
+//     const reveredArr = [];
+//     let i=arr.length-1;
+//     while(i>=0){
+//         reveredArr.push(arr[i]);
+//         i--;
+//     }
+//     console.log(reveredArr);
+// }
+
+
+// third way
+
+// function reverseArrayExtraArray(arr){
+//     console.log(arr.reverse());
+// }
+
+
+// fourth way
+
+function reverseArrayExtraArray(arr){
+    
+}
+
+const originalArr = [1, 2, 3, 4, 5];
+reverseArrayExtraArray(originalArr);
