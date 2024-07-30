90fcad33913dcdf5fddbf97ca11e7d71376adff6
Nitesh-Samaniya <niteshsamaniya97@gmail.com>
2024-07-31 01:58:19 +0530

    sort_0_1_2

---
+++ array/4.sort_0_1_2.js
@@ -0,0 +1,39 @@
+// Sort 0s, 1s and 2s
+
+const N = 5;
+const arr = [0, 2, 1, 2, 0];
+
+function swap(arr, i, j){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function sort012(arr, N){
+    let low = 0;
+        let mid = 0;
+        let high = N-1;
+        
+        function swap(arr, i, j){
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        
+        while(mid <= high){
+            if(arr[mid] === 0){
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            }else if(arr[mid] === 1){
+                mid++;
+            }else{
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+        
+        return arr;
+}
+
+console.log(sort012(arr, N))
